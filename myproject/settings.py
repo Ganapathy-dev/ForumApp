79aa9c5e94f54ae3d2e076f08a2dc0e2bf053104
Ganapathy <ganapathy@testpress.in>
2024-11-05 16:24:22 +0530

    Forum App part 3 & 4 commit
    - created user login, signup, password reset view and template
    with that automated testcases

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'boards',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -116,9 +118,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    BASE_DIR/"static",
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL='home'
+LOGIN_REDIRECT_URL='home'
+
+LOGIN_URL='login'
+
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
